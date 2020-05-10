e1ec4884081fe3c49ded2ec0b537c7cc8c9b154d
LautaroCosso <64047426+LautaroCosso@users.noreply.github.com>
2020-05-10 11:41:52 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
+class Cronometro extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      tiempoTrabajo = 25,
+      tiempoDescanso = 5,
+      segundos= 0,
+      formato:'00:00',
+    }
+  }
+  start(){
+   //No supe hacerlo funcionar
+  }
+
+  timeFormater(time) {
+    if (time < 10) {
+        time = '0' + time
+    }
+    return time
+}
+
+  render() {
+    return (
+          <Text style={{color: this.state.color}}> {this.state.timeFormat} </Text>
+    );
+  }
+}
+ 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Bienvenidos NT2!</Text>
       <Text>Proyecto a realizar: Cronometro Pomodoro.</Text>
+      <Text>Disculpe Profe, no supe como hacerlo y no regunté en su debido tiempo. Lautaro Cosso.</Text>
     </View>
   );
 }
